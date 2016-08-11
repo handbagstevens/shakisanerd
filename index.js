8060900318068395400f9879792a9337a9555ac3
Jamie Yung <jamie.yung286@gmail.com>
2016-08-11 15:18:59 +1000

    Made the parts show one by one.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function init () {
+	$('#yesbtn').click(function () {
+		$('#part2').show();
+	});
+
     $('#whonerdselect').change(function () {
         $('#whonerdselect option[value=pls]').remove();
         $('#whonerdtext').text('Wow rly what a nerd lel');
+        $('#part3').show();
     });
 
     $('#revealnerdbtn').click(function () {
-        $('#revealnerdtext').append('Shak is a nerd ');
+    	var el = $('#revealnerdtext');
+    	el.text(el.text() + 'Shak is a nerd ');
     });
 }
