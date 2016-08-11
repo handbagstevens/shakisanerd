9fb9a742012e02c62225c3d2c8eb6ba5d4173876
Jamie Yung <jamie.yung286@gmail.com>
2016-08-11 15:49:14 +1000

    Added comment section.

--- index.js
+++ index.js
@@ -12,5 +12,35 @@ function init () {
     $('#revealnerdbtn').click(function () {
     	var el = $('#revealnerdtext');
     	el.text(el.text() + 'Shak is a nerd ');
+        $('#part4').show();
+    });
+
+    function repeatStrToLen (str, len) {
+        var n = Math.ceil(len / str.length);
+        var ret = str.repeat(n);
+        return ret.slice(0, len);
+    }
+
+    var nameinput = $('#nameinput')
+    nameinput.on('change keydown paste', function () {
+        setTimeout(function () {
+            var len = nameinput.val().length;
+            var newVal = repeatStrToLen('Shak', Math.min(len, 4));
+            nameinput.val(newVal);
+        });
+    });
+
+    var commentinput = $('#commentinput');
+    commentinput.on('change keydown paste', function () {
+        setTimeout(function () {
+            var len = commentinput.val().length;
+            $('#commentbtn').prop('disabled', !len);
+            var newVal = repeatStrToLen('I am a nerd ', len);
+            commentinput.val(newVal);
+        });
+    });
+
+    $('#commentbtn').click(function () {
+        $('#commenttext').text('omg nerd imo');
     });
 }
